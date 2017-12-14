24ea09e89852dabfcf688e6f1ec92c4e6cb5e6da
bkozhaev@gmail.com <bkozhaev@gmail.com>
2017-12-14 10:14:33 +0400

    самое последнее исправление 03 css и js не минимизированы

--- js/gllacy.js
+++ js/gllacy.js
@@ -1,21 +1,23 @@
 var connectBtn = document.querySelector(".location-button");
 var popup = document.querySelector(".modal-wrapper");
-var formPopup = document.querySelector(".modal-feedback")
+var formPopup = popup.querySelector(".modal-feedback")
 var closeBtn = popup.querySelector(".close-button");
 var login = popup.querySelector("[name=login]");
 var form = popup.querySelector("form");
 var email = popup.querySelector("[name=email-2]")
 var storage = localStorage.getItem("login");
 var body = document.querySelector(".body");
-var slideBtn_1 = body.querySelector(".slide-1");
-var slideBtn_2 = body.querySelector(".slide-2");
-var slideBtn_3 = body.querySelector(".slide-3");
-var slide_1 = body.querySelector(".slide-block-1");
-var slide_2 = body.querySelector(".slide-block-2");
-var slide_3 = body.querySelector(".slide-block-3");
+if (body){
+  var slideBtn_1 = body.querySelector(".slide-1");
+  var slideBtn_2 = body.querySelector(".slide-2");
+  var slideBtn_3 = body.querySelector(".slide-3");
+  var slide_1 = body.querySelector(".slide-block-1");
+  var slide_2 = body.querySelector(".slide-block-2");
+  var slide_3 = body.querySelector(".slide-block-3");
+}
 
 
-
+if(slideBtn_1) {
 slideBtn_1.addEventListener("click", function(evt){
   evt.preventDefault();
   slideBtn_1.classList.add("slide-button-current");
@@ -25,8 +27,10 @@ slideBtn_1.addEventListener("click", function(evt){
   slide_2.classList.remove("active");
   slide_3.classList.remove("active");
   slide_1.classList.add("active");
-})
+});
+}
 
+if(slideBtn_2){
 slideBtn_2.addEventListener("click", function(evt){
   evt.preventDefault();
   slideBtn_2.classList.add("slide-button-current");
@@ -36,37 +40,46 @@ slideBtn_2.addEventListener("click", function(evt){
   slide_1.classList.remove("active");
   slide_3.classList.remove("active");
   slide_2.classList.add("active");
-})
+});
+}
 
-slideBtn_3.addEventListener("click", function(evt){
-  evt.preventDefault();
-  slideBtn_3.classList.add("slide-button-current");
-  slideBtn_1.classList.remove("slide-button-current");
-  slideBtn_2.classList.remove("slide-button-current");
-  body.style.backgroundColor = "#9d8b84";
-  slide_2.classList.remove("active");
-  slide_1.classList.remove("active");
-  slide_3.classList.add("active");
-})
+if(slideBtn_3){
+  slideBtn_3.addEventListener("click", function(evt){
+    evt.preventDefault();
+    slideBtn_3.classList.add("slide-button-current");
+    slideBtn_1.classList.remove("slide-button-current");
+    slideBtn_2.classList.remove("slide-button-current");
+    body.style.backgroundColor = "#9d8b84";
+    slide_2.classList.remove("active");
+    slide_1.classList.remove("active");
+    slide_3.classList.add("active");
+  });
+}
 
+if(connectBtn){
 connectBtn.addEventListener("click", function(evt){
   evt.preventDefault();
   popup.classList.add("modal-show");
-  formPopup.classList.add("modal-bounce");
-  if (storage) {
-    login.value = storage;
-    email.focus();
-  } else {
-  login.focus();
+  if(formPopup){formPopup.classList.add("modal-bounce");
+    if (storage) {
+      login.value = storage;
+      email.focus();
+    } else {
+    login.focus();
+    }
   }
-})
+});
+}
 
+if(closeBtn) {
 closeBtn.addEventListener("click", function(evt){
   evt.preventDefault();
   popup.classList.remove("modal-show");
   formPopup.classList.remove("modal-error");
-})
+});
+}
 
+if(form) {
 form.addEventListener("submit", function(evt){
   if (!login.value || !email.value) {
     evt.preventDefault();
@@ -76,7 +89,8 @@ form.addEventListener("submit", function(evt){
   } else {
     localStorage.setItem("login", login.value);
   }
-})
+});
+}
 
 window.addEventListener("keydown", function (evt){
   if (evt.keyCode === 27) {
